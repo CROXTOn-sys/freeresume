dad5b69c0590531b8802ce783982fdf5860d0e51
Rafi shaik <rafisk92851@gmail.com>
2026-07-06 13:58:10 +0530

    .

--- src/app/api/resume/route.js
+++ src/app/api/resume/route.js
@@ -16,17 +16,13 @@ async function getBrowser() {
     if (envPath) {
       return await puppeteer.launch({
         executablePath: envPath,
-        headless: 'new',
+        headless: true,
         args: [
           '--no-sandbox',
           '--disable-setuid-sandbox',
           '--disable-gpu',
           '--disable-dev-shm-usage',
-          '--disable-software-rasterizer',
           '--no-zygote',
-          '--disable-extensions',
-          '--disable-background-networking',
-          '--disable-dbus',
         ],
       });
     }
@@ -35,7 +31,7 @@ async function getBrowser() {
     try {
       const execPath = await chromium.executablePath();
       return await puppeteer.launch({
-        args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--no-zygote', '--disable-dbus'],
+        args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--no-zygote'],
         defaultViewport: chromium.defaultViewport,
         executablePath: execPath,
         headless: chromium.headless,
@@ -48,8 +44,8 @@ async function getBrowser() {
       if (found) {
         return await puppeteer.launch({
           executablePath: found,
-          headless: 'new',
-          args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--no-zygote', '--disable-dbus'],
+          headless: true,
+          args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--no-zygote'],
         });
       }
       throw err;
